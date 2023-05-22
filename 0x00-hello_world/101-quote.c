7325bd101732c2c7862050a2ad2ed67bb44c3994
Emmanuel Oyibo <emminexworld@gmail.com>
2023-05-22 12:33:14 +0100

    Add a C program that prints exactly
    and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
    - You are not allowed to use any functions listed in the NAME section of the man (3) printf or man (3) puts
    - Your program should return 1
    - Your program should compile without any warnings when using the -Wall gcc option

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,11 @@
 int main(void)
 {
 	int fd = 2;
-	const void *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	size_t count = strlen(message);
+	const void *message;
+	size_t count;
 
+	message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	count = strlen(message);
 	write(fd, message, count);
 	return (1);
 }
