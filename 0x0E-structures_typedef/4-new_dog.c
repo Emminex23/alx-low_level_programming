443f0873486f820aff9c5164632d6aa7890082d8
Emmanuel Oyibo <emminexworld@gmail.com>
2023-08-16 01:15:26 +0100

    Add a function that creates a new dog

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,75 @@
+#include "dog.h"
+/**
+ * _strlen - returns the length of a str
+ * @str: pointer to the str
+ *
+ * Return: int
+ */
+int _strlen(char *str)
+{
+	int i, sum = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		sum += 1;
+	}
+	return (sum);
+}
+/**
+ * _strdup - function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the
+ * string given as a parameter.
+ * @str: the string
+ *
+ * Return: pointer to a new string which is a duplicate of the string str
+ * NULL if str = NULL
+ * NULL if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr = (char *) malloc(_strlen(str) + 1);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+/**
+ * new_dog - function that creates a new dog.
+ * @name: first element
+ * @age: second element
+ * @owner: third element
+ *
+ * Return: ptr
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *ptr1;
+
+	ptr1 = (dog_t *) malloc(sizeof(dog_t));
+	if (ptr1 == NULL)
+	{
+		return (NULL);
+	}
+
+	ptr1->name = _strdup(name);
+	ptr1->age = age;
+	ptr1->owner = _strdup(owner);
+
+	return (ptr1);
+}
