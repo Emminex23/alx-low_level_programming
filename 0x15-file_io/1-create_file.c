754cbcd41c44a6a36aaf7109749f0a0f8036e05f
Emmanuel Oyibo <emminexworld@gmail.com>
2023-09-16 16:14:45 +0100

    Add a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int _strlen(char *str)
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, written_bytes;
 
 	if (filename == NULL)
 	{
@@ -33,20 +33,22 @@ int create_file(const char *filename, char *text_content)
 
 	if (!text_content)
 	{
-		open(filename, O_RDONLY | O_CREAT, 0600);
-	}
-
-	if (text_content)
-	{
-		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		fd = open(filename, O_RDONLY | O_CREAT, 0600);
 		if (fd == -1)
 		{
 			return (-1);
 		}
-
-		write(fd, text_content, _strlen(text_content));
 		close(fd);
 		return (1);
 	}
-	return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	written_bytes = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	return ((written_bytes == -1) ? -1 : 1);
 }
