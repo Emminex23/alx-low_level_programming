7a0ae8c9cd16bf57fa26f506c2b022205329eb3b
Emmanuel Oyibo <emminexworld@gmail.com>
2023-09-16 15:29:51 +0100

    Add a function that creates a file.

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * _strlen - length of the string
+ * @str: the string
+ *
+ * Return: length of the string
+ */
+int _strlen(char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(str + 1));
+}
+
+/**
+ * create_file - function that creates a file.
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	if (!text_content)
+	{
+		open(filename, O_RDONLY | O_CREAT, 0600);
+	}
+
+	if (text_content)
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+		if (fd == -1)
+		{
+			return (-1);
+		}
+
+		write(fd, text_content, _strlen(text_content));
+		close(fd);
+		return (1);
+	}
+	return (-1);
+}
